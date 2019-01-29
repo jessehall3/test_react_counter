fd6edaab09b3a1e8a78b95da1728b536a1c54d86
Jesse Hall <jessehall3@gmail.com>
2019-01-29 10:31:47 -0500

    Counter works.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+import Button from './components/Button'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function App() {
+  const [count, setCount] = useState(0)
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  const incrementCount = increment => {
+    setCount(count + increment)
+  }
+
+  return (
+    <div className="App">
+      <Button increment={1} onClickFunction={incrementCount} />
+      <Button increment={10} onClickFunction={incrementCount} />
+      <Button increment={100} onClickFunction={incrementCount} />
+      <Button increment={1000} onClickFunction={incrementCount} />
+      <span>{count}</span>
+    </div>
+  )
+}
+
+const rootElement = document.getElementById('root')
+ReactDOM.render(<App />, rootElement)
